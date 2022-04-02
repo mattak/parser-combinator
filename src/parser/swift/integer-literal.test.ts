6829b668b63a27af4601ca01ee156fb91dc1b1f5
mattak <mattak.me@gmail.com>
2022-04-02 16:12:05 +0900

    :seedling: define integer-literal of swift

---
+++ src/parser/swift/integer-literal.test.ts
@@ -0,0 +1,368 @@
+import {ParserOutput} from "../../types";
+import {binaryLiteral, decimalLiteral, hexadecimalLiteral, integerLiteral, octalLiteral} from "./integer-literal";
+
+describe('binaryLiteral', () => {
+  const parser = binaryLiteral;
+
+  test('Empty input', () => {
+    const input = [] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'fail'
+    });
+  });
+
+  test('input "a"', () => {
+    const input = [...'a'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'fail',
+    });
+  });
+
+  test('input "0b"', () => {
+    const input = [...'0b'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'fail',
+    });
+  });
+
+
+  test('input "0b0"', () => {
+    const input = [...'0b0'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'success',
+      data: 0,
+      rest: [],
+    });
+  });
+
+  test('input "0b1"', () => {
+    const input = [...'0b1'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'success',
+      data: 1,
+      rest: [],
+    });
+  });
+
+  test('input "0b0_1"', () => {
+    const input = [...'0b0_1'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'success',
+      data: 1,
+      rest: [],
+    });
+  });
+
+  test('input "0b00_11_00"', () => {
+    const input = [...'0b00_11_00'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'success',
+      data: 12,
+      rest: [],
+    });
+  });
+});
+
+describe('octalLiteral', () => {
+  const parser = octalLiteral;
+
+  test('Empty input', () => {
+    const input = [] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'fail'
+    });
+  });
+
+  test('input "a"', () => {
+    const input = [...'a'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'fail',
+    });
+  });
+
+  test('input "0o"', () => {
+    const input = [...'0o'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'fail',
+    });
+  });
+
+
+  test('input "0o0"', () => {
+    const input = [...'0o0'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'success',
+      data: 0,
+      rest: [],
+    });
+  });
+
+  test('input "0o7"', () => {
+    const input = [...'0o1'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'success',
+      data: 1,
+      rest: [],
+    });
+  });
+
+  test('input "0o8"', () => {
+    const input = [...'0o8'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'fail',
+    });
+  });
+
+  test('input "0o0_1"', () => {
+    const input = [...'0o0_1'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'success',
+      data: 1,
+      rest: [],
+    });
+  });
+
+  test('input "0o00_01_00"', () => {
+    const input = [...'0o00_01_00'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'success',
+      data: 64,
+      rest: [],
+    });
+  });
+});
+
+describe('decimalLiteral', () => {
+  const parser = decimalLiteral;
+
+  test('Empty input', () => {
+    const input = [] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'fail'
+    });
+  });
+
+  test('input "a"', () => {
+    const input = [...'a'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'fail',
+    });
+  });
+
+  test('input "0"', () => {
+    const input = [...'0'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'success',
+      data: 0,
+      rest: [],
+    });
+  });
+
+
+  test('input "00"', () => {
+    const input = [...'00'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'success',
+      data: 0,
+      rest: [],
+    });
+  });
+
+  test('input "12"', () => {
+    const input = [...'12'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'success',
+      data: 12,
+      rest: [],
+    });
+  });
+
+  test('input "0_123"', () => {
+    const input = [...'0_123'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'success',
+      data: 123,
+      rest: [],
+    });
+  });
+
+  test('input "1_234_56"', () => {
+    const input = [...'1_234_56'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'success',
+      data: 123456,
+      rest: [],
+    });
+  });
+});
+
+describe('hexadecimalLiteral', () => {
+  const parser = hexadecimalLiteral;
+
+  test('Empty input', () => {
+    const input = [] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'fail'
+    });
+  });
+
+  test('input "a"', () => {
+    const input = [...'a'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'fail',
+    });
+  });
+
+  test('input "0x"', () => {
+    const input = [...'0x'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'fail',
+    });
+  });
+
+
+  test('input "0x0"', () => {
+    const input = [...'0x0'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'success',
+      data: 0,
+      rest: [],
+    });
+  });
+
+  test('input "0x10"', () => {
+    const input = [...'0x10'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'success',
+      data: 16,
+      rest: [],
+    });
+  });
+
+  test('input "0xf"', () => {
+    const input = [...'0xf'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'success',
+      data: 15,
+      rest: [],
+    });
+  });
+
+  test('input "0xE"', () => {
+    const input = [...'0xE'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'success',
+      data: 14,
+      rest: [],
+    });
+  });
+
+  test('input "0x9_F"', () => {
+    const input = [...'0x9_F'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'success',
+      data: 159,
+      rest: [],
+    });
+  });
+
+  test('input "0xFF_ff_FF"', () => {
+    const input = [...'0xFF_ff_FF'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'success',
+      data: 16777215,
+      rest: [],
+    });
+  });
+});
+
+describe('integerLiteral', () => {
+  const parser = integerLiteral;
+
+  test('Empty input', () => {
+    const input = [] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'fail'
+    });
+  });
+
+  test('input "a"', () => {
+    const input = [...'a'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'fail',
+    });
+  });
+
+  test('input "0b10"', () => {
+    const input = [...'0b10'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'success',
+      data: 2,
+      rest: [],
+    });
+  });
+
+  test('input "0o10"', () => {
+    const input = [...'0o10'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'success',
+      data: 8,
+      rest: [],
+    });
+  });
+
+  test('input "10"', () => {
+    const input = [...'10'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'success',
+      data: 10,
+      rest: [],
+    });
+  });
+
+  test('input "0x10"', () => {
+    const input = [...'0x10'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<number>>({
+      result: 'success',
+      data: 16,
+      rest: [],
+    });
+  });
+});
---
+++ src/parser/swift/integer-literal.ts
@@ -0,0 +1,97 @@
+import {Parser} from "../../types";
+import {cat, or, rep} from "../../combinators";
+import {map, opt, str} from "../../util";
+import {char, Digit, digit, is} from "../../char";
+
+type BinaryDigit = '0' | '1';
+type BinaryLiteralCharacter = BinaryDigit | '_';
+
+// <binary-digit> ::= 0 | 1
+// <binary-literal-character> ::= <binary-digit> | _
+// <binary-literal> ::= '0b' <binary-digit> <octal-literal-character>*
+const binaryDigit: Parser<BinaryDigit> = or([char('0'), char('1')]);
+const binaryLiteralCharacter: Parser<BinaryLiteralCharacter> = or([
+  binaryDigit,
+  char('_')
+]);
+export const binaryLiteral: Parser<number> = map(
+  cat([
+    str('0b'),
+    binaryDigit,
+    rep(binaryLiteralCharacter),
+  ]),
+  ([prefix, first, last]) => {
+    const dlast = last.join('').replace(/_/g, '');
+    const digits = first + dlast;
+    return Number.parseInt(digits, 2);
+  }
+);
+
+type OctalDigit = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7';
+type OctalLiteralCharacter = OctalDigit | '_';
+
+// <octal-digit> ::= 0 | 1 | ... | 7
+// <octal-literal-character> ::= <octal-literal> | _
+// <octal-literal> ::= '0o' <octal-digit> <octal-literal-character>*
+const octalDigit: Parser<OctalDigit> = is((c): c is OctalDigit => /[0-7]/.test(c));
+const octalLiteralCharacter: Parser<OctalLiteralCharacter> = is((c): c is OctalLiteralCharacter => /[0-7_]/.test(c));
+export const octalLiteral: Parser<number> = map(
+  cat([
+    str('0o'),
+    octalDigit,
+    rep(octalLiteralCharacter),
+  ]),
+  ([prefix, first, last]) => {
+    const dlast = last.join('').replace(/_/g, '');
+    const digits = first + dlast;
+    return Number.parseInt(digits, 8);
+  }
+);
+
+// <decimal-digit> ::= 0 | ... | 9
+// <decimal-literal-character> ::= <decimal-digit> | _
+// <decimal-literal> ::= <decimal-digit> <decimal-literal-character>*
+type DecimalDigit = Digit;
+type DecimalLiteralCharacter = Digit | '_';
+const decimalDigit: Parser<DecimalDigit> = digit;
+export const decimalDigits: Parser<DecimalDigit[]> = rep(decimalDigit, 1);
+const decimalLiteralCharacter = is((c): c is DecimalLiteralCharacter => /[0-9_]/.test(c));
+export const decimalLiteral: Parser<number> = map(
+  cat([
+    decimalDigit,
+    rep(decimalLiteralCharacter),
+  ]),
+  ([first, last]) => {
+    const dlast = last.join('').replace(/_/g, '');
+    const digits = first + dlast;
+    return Number.parseInt(digits, 10);
+  }
+);
+
+// <hexadecimal-digit> ::= 0 | ... | 9
+// <hexadecimal-literal-character> ::= <hexadecimal-digit> | _
+// <hexadecimal-literal> ::= <hexadecimal-digit> <hexadecimal-literal-character>*
+type HexadecimalDigit = Digit | 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'A' | 'B' | 'C' | 'D' | 'E' | 'F';
+type HexadecimalLiteralCharacter = HexadecimalDigit | '_';
+const hexadecimalDigit: Parser<HexadecimalDigit> = is((c): c is HexadecimalDigit => /[0-9a-fA-F]/.test(c));
+const hexadecimalLiteralCharacter: Parser<HexadecimalLiteralCharacter> = is((c): c is HexadecimalLiteralCharacter => /[0-9a-fA-F_]/.test(c));
+export const hexadecimalLiteral: Parser<number> = map(
+  cat([
+    str('0x'),
+    hexadecimalDigit,
+    rep(hexadecimalLiteralCharacter)
+  ]),
+  ([prefix, first, last]) => {
+    const dlast = last.join('').replace(/_/g, '');
+    const digits = first + dlast;
+    return Number.parseInt(digits, 16);
+  }
+);
+
+// <integer-literal> ::= <binary-literal> | <octal-literal> | <hexadecimal-literal> | <decimal-literal>
+export const integerLiteral: Parser<number> = or([
+  binaryLiteral,
+  octalLiteral,
+  hexadecimalLiteral,
+  decimalLiteral,
+]);
