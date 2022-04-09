fd4cab0e9f382c5bc64377ced2dcdd3da65c63bd
mattak <mattak.me@gmail.com>
2022-04-09 14:04:01 +0900

    :seedling: define declaration-modifier

---
+++ src/parser/swift/declaration/declaration-modifier.test.ts
@@ -0,0 +1,82 @@
+import {declarationModifier, SwiftDeclarationModifier} from "./declaration-modifier";
+import {ParserOutput} from "../../../types";
+
+describe('declaration-modifier', () => {
+  const parser = declarationModifier;
+
+  test('Input empty', () => {
+    const input = [] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<SwiftDeclarationModifier>>({
+      result: 'fail',
+    });
+  });
+
+  test('Input a', () => {
+    const input = [...'a'] as const;
+    const output = parser(input);
+    expect(output).toEqual<ParserOutput<SwiftDeclarationModifier>>({
+      result: 'fail',
+    });
+  });
+
+  const words = [
+    "class",
+    "convenience",
+    "dynamic",
+    "final",
+    "infix",
+    "lazy",
+    "optional",
+    "override",
+    "postfix",
+    "prefix",
+    "required",
+    "static",
+    "unowned",
+    "unowned(safe)",
+    "unowned(unsafe)",
+    "weak",
+    "private", "private(set)",
+    "fileprivate", "fileprivate(set)",
+    "internal", "internal(set)",
+    "public", "public(set)",
+    "open", "open(set)",
+    "mutating", "nonmutating",
+    "nonisolated",
+  ];
+  for (let word of words) {
+    test(`Input ${word}`, () => {
+      const input = [...`${word}`] as const;
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<string>>({
+        result: 'success',
+        data: `${word}`,
+        rest: [],
+      });
+    });
+  }
+
+  const word2correct: { [word: string]: string } = {
+    "unowned ( safe )": "unowned(safe)",
+    "unowned ( unsafe )": "unowned(unsafe)",
+    "private ( set )": "private(set)",
+    "fileprivate ( set )": "fileprivate(set)",
+    "internal ( set )": "internal(set)",
+    "public ( set )": "public(set)",
+    "open ( set )": "open(set)",
+  }
+  Object.keys(word2correct).forEach(word => {
+    let correct = word2correct[word];
+    test(`Input ${word}`, () => {
+      const input = [...`${word}`] as const;
+      const output = parser(input);
+      expect(output).toEqual<ParserOutput<string>>({
+        result: 'success',
+        data: `${correct}`,
+        rest: [],
+      });
+    });
+  });
+
+});
---
+++ src/parser/swift/declaration/declaration-modifier.ts
@@ -0,0 +1,108 @@
+import {Parser} from "../../../types";
+import {cat, or} from "../../../combinators";
+import {map, str} from "../../../util";
+import {char} from "../../../char";
+import {whitespace0} from "../lexical-struct/whitespace";
+
+type SwiftDeclarationModifierPrimitive =
+  'class'
+  | 'convenience'
+  | 'dynamic'
+  | 'final'
+  | 'infix'
+  | 'lazy'
+  | 'optional'
+  | 'override'
+  | 'postfix'
+  | 'prefix'
+  | 'required'
+  | 'static'
+  | 'unowned'
+  | 'unowned(safe)'
+  | 'unowned(unsafe)'
+  | 'weak';
+
+export const declarationModifierPrimitive: Parser<SwiftDeclarationModifierPrimitive> = or([
+  str('class'),
+  str('convenience'),
+  str('dynamic'),
+  str('final'),
+  str('infix'),
+  str('lazy'),
+  str('optional'),
+  str('override'),
+  str('postfix'),
+  str('prefix'),
+  str('required'),
+  str('static'),
+  map(
+    cat([str('unowned'), whitespace0, char('('), whitespace0, str('safe'), whitespace0, char(')')]),
+    (s) => 'unowned(safe)',
+  ),
+  map(
+    cat([str('unowned'), whitespace0, char('('), whitespace0, str('unsafe'), whitespace0, char(')')]),
+    (s) => 'unowned(unsafe)',
+  ),
+  str('unowned'),
+  str('weak'),
+]);
+
+type SwiftAccessLevelModifier =
+  'private' | 'private(set)'
+  | 'fileprivate' | 'fileprivate(set)'
+  | 'internal' | 'internal(set)'
+  | 'public' | 'public(set)'
+  | 'open' | 'open(set)'
+  ;
+
+export const accessLevelModifier: Parser<SwiftAccessLevelModifier> = or([
+  map(
+    cat([str('private'), whitespace0, char('('), whitespace0, str('set'), whitespace0, char(')')]),
+    (s) => 'private(set)',
+  ),
+  map(
+    cat([str('fileprivate'), whitespace0, char('('), whitespace0, str('set'), whitespace0, char(')')]),
+    (s) => 'fileprivate(set)',
+  ),
+  map(
+    cat([str('internal'), whitespace0, char('('), whitespace0, str('set'), whitespace0, char(')')]),
+    (s) => 'internal(set)',
+  ),
+  map(
+    cat([str('public'), whitespace0, char('('), whitespace0, str('set'), whitespace0, char(')')]),
+    (s) => 'public(set)',
+  ),
+  map(
+    cat([str('open'), whitespace0, char('('), whitespace0, str('set'), whitespace0, char(')')]),
+    (s) => 'open(set)',
+  ),
+  str('private'),
+  str('fileprivate'),
+  str('internal'),
+  str('internal'),
+  str('public'),
+  str('open'),
+]);
+
+type SwiftMutationModifier = 'mutating' | 'nonmutating';
+
+export const mutatingModifier: Parser<SwiftMutationModifier> = or([
+  str('mutating'),
+  str('nonmutating')
+]);
+
+type SwiftActorIsolationModifier = 'nonisolated';
+
+export const actorIsolationModifier: Parser<SwiftActorIsolationModifier> = str('nonisolated');
+
+export type SwiftDeclarationModifier =
+  SwiftDeclarationModifierPrimitive
+  | SwiftAccessLevelModifier
+  | SwiftMutationModifier
+  | SwiftActorIsolationModifier;
+export const declarationModifier: Parser<SwiftDeclarationModifier> = or([
+  declarationModifierPrimitive,
+  accessLevelModifier,
+  mutatingModifier,
+  actorIsolationModifier,
+]);
