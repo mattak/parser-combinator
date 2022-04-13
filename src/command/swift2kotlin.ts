cff6157cecd4b6de08d7d212922eff1f9efeb26f
mattak <mattak.me@gmail.com>
2022-04-13 14:29:49 +0900

    :seedling: create swift2kotlin command

---
+++ src/command/swift2kotlin.ts
@@ -0,0 +1,41 @@
+import * as fs from "fs";
+import {topLevelDeclaration} from "../parser/swift/declaration/top-level-declaration";
+import {fileConverter} from "../converter/swift2kotlin/general/file";
+import {KotlinFile} from "../syntax/kotlin";
+import {kotlinFilePrinter} from "../printer/kotlin/general/file";
+import * as util from "util";
+
+async function main() {
+  if (process.argv.length <= 2) {
+    console.log('usage: <filepath>')
+    process.exit(1);
+    return
+  }
+
+  const [, , filepath] = process.argv;
+
+  const text = await fs.promises.readFile(filepath, 'utf8');
+  const input = text.split('');
+  const parsed = topLevelDeclaration(input);
+  console.log(`parsed: ${util.inspect(parsed)}`);
+
+  if (parsed.result === 'fail') {
+    console.error('parse failed');
+    process.exit(1);
+    return;
+  }
+
+  const kotlin = fileConverter(parsed.data, <KotlinFile>{
+    packageHeader: 'com.example'
+  });
+
+  const result = kotlinFilePrinter({indentLevel: 0, data: kotlin});
+  console.log(result.join('\n'));
+}
+
+main()
+  .then()
+  .catch(x => {
+    console.log(x);
+    process.exit(1);
+  });
